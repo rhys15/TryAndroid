be15ab14da878212e82154d07a564925e46cc66f
Rhys Gabion <rhys15gabion@gmail.com>
2013-05-30 01:17:28 +0800

    trycommit

--- TryAndroid/src/com/example/tryandroid/MainActivity.java
+++ TryAndroid/src/com/example/tryandroid/MainActivity.java
@@ -5,11 +5,12 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+	private String name = "hi mate dwyane";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 
 
